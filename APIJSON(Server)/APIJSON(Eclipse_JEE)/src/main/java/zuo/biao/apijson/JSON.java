92a565283cbda2255d36c191ea1bb8bb801b2b2f
TommyLemon <1184482681@qq.com>
2016-12-07 21:44:04 +0800

    新增APIJSON(Server)/eclipse_jee工程

---
+++ APIJSON(Server)/APIJSON(Eclipse_JEE)/src/main/java/zuo/biao/apijson/JSON.java
@@ -0,0 +1,118 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package zuo.biao.apijson;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import java.util.List;
+
+/**阿里json封装类 防止解析时异常
+ * @author Lemon
+ */
+public class JSON {
+	private static final String TAG = "JSON";
+
+	/**判断json格式是否正确
+	 * @param s
+	 * @return
+	 */
+	public static boolean isJsonCorrect(String s) {
+		System.out.println(TAG + "isJsonCorrect  <<<<     " + s + "     >>>>>>>");
+		if (s == null || s.equals("[]") 
+				|| s.equals("{}") || s.equals("") || s.equals("[null]") || s.equals("{null}") || s.equals("null")) {
+			return false;
+		}
+		return true;
+	}
+
+	/**获取有效的json
+	 * @param s
+	 * @return
+	 */
+	public static String getCorrectJson(String s) {
+		s = StringUtil.getNoBlankString(s);
+		return isJsonCorrect(s) ? s : "";
+	}
+
+	/**json转JSONObject
+	 * @param json
+	 * @return
+	 */
+	public static JSONObject parseObject(String json) {
+		try {
+			int features = com.alibaba.fastjson.JSON.DEFAULT_PARSER_FEATURE;
+			features |= Feature.SortFeidFastMatch.getMask();
+			return com.alibaba.fastjson.JSON.parseObject(getCorrectJson(json), JSONObject.class, features);
+		} catch (Exception e) {
+			System.out.println(TAG + "parseObject  catch \n" + e.getMessage());
+		}
+		return null;
+	}
+
+	/**json转实体类
+	 * @param json
+	 * @param clazz
+	 * @return
+	 */
+	public static <T> T parseObject(String json, Class<T> clazz) {
+		try {
+			int features = com.alibaba.fastjson.JSON.DEFAULT_PARSER_FEATURE;
+			features |= Feature.SortFeidFastMatch.getMask();
+			return com.alibaba.fastjson.JSON.parseObject(getCorrectJson(json), clazz, features);
+		} catch (Exception e) {
+			System.out.println(TAG + "parseObject  catch \n" + e.getMessage());
+		}
+		return null;
+	}
+
+	/**json转JSONArray
+	 * @param json
+	 * @return
+	 */
+	public static JSONArray parseArray(String json) {
+		return  com.alibaba.fastjson.JSON.parseArray(json);
+	}
+	/**json转实体类列表
+	 * @param json
+	 * @param clazz
+	 * @return
+	 */
+	public static <T> List<T> parseArray(String json, Class<T> clazz) {
+		try {
+			return com.alibaba.fastjson.JSON.parseArray(getCorrectJson(json), clazz);
+		} catch (Exception e) {
+			System.out.println(TAG + "parseArray  catch \n" + e.getMessage());
+		}
+		return null;
+	}
+
+	/**实体类转json
+	 * @param obj
+	 * @return
+	 */
+	public static String toJSONString(Object obj) {
+		try {
+			return com.alibaba.fastjson.JSON.toJSONString(obj, SerializerFeature.SortField);
+		} catch (Exception e) {
+			System.out.println(TAG + "toJSONString  catch \n" + e.getMessage());
+		}
+		return null;
+	}
+
+
+}
---
+++ APIJSON(Server)/APIJSON(Eclipse_JEE)/src/main/java/zuo/biao/apijson/server/ClientTest.java
@@ -0,0 +1,80 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package zuo.biao.apijson.server;
+
+/**mock test of client
+ * @author Lemon
+ */
+public class ClientTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//		new RequestParser().get(\"{User\\":{\\"id\\":1}}\");
+		//		
+		//		new RequestParser().get(\"{[User]\\":{ \\"range\\":1, \\"pageNum\\":1,  \\"pageSize\\":10,  \\"sortOrder\\":\\"id\\",  \\"sortDirection\\":\\"increase\\",}}\");
+		//		
+		//		new RequestParser().get(\"{\\"[User, Work, [picture]]\\": { \\"=\\": { \\"User\\": id,  \\"Work\\": \\"userId\\",  \\"picture\\": \\"userId\\"}\\"pageNum\\": 1, \\"pageSize\\": 10, \\"sortOrder\\": \\"id\\", \\"sortDirection\\": \\"increase\\", \\"User\\": {  \\"sex\\":SEX_FEMAIL, } \\"picture\\":{ \\"pageNum\\":1, \\"pageSize\\": 3, }}\");
+
+
+//		//已经过bejson校验和传输校验
+//		new RequestParser().parse("{" +
+////				     "\"[]\": {" +
+////				        "\"page\": 1, " +
+////				        "\"count\": 5," +
+//				        " \"User\": {" +
+//				               "\"id\": 38710," +//70793
+////				               "\"range\": 1," +
+////				               "\"sex\": 0" +
+//				         "}," +
+//				         "\"Work\": {" +
+//				               "\"userId\": \"User/id\"" +
+//				         "}," +
+//				        "\"Comment[]\": {" +
+//				              "\"page\": 0," +
+//				              "\"count\": 3," +
+//				              " \"Comment\": {" +
+//				                   "\"workId\": \"Work/id\"" +
+//		                      "}," +
+//				         "}" +
+////				    "}" +
+//		"}");
+		
+		//已经过bejson校验和传输校验
+		new RequestParser().parse("{" +
+				     "\"[]\": {" +
+				        "\"page\": 1, " +
+				        "\"count\": 10," +
+				        " \"User\": {" +
+				               "\"sex\": 0" +
+				         "}," +
+				         "\"Work\": {" +
+				               "\"userId\": \"/User/id\"" +
+				         "}," +
+				        "\"Comment[]\": {" +
+				              "\"page\": 0," +
+				              "\"count\": 3," +
+				              " \"Comment\": {" +
+				                   "\"workId\": \"[]/Work/id\"" +
+		                      "}," +
+				         "}" +
+				    "}" +
+		"}");
+
+
+	}
+
+}
