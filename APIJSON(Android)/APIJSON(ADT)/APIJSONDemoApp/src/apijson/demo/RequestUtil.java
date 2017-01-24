e752d32d4725800a6bd4d343ad83d23a2e7056ef
TommyLemon <1184482681@qq.com>
2017-01-24 16:36:52 +0800

    RequestUtil请求改为和显示一致

--- APIJSON(Android)/APIJSON(ADT)/APIJSONDemoApp/src/apijson/demo/RequestUtil.java
+++ APIJSON(Android)/APIJSON(ADT)/APIJSONDemoApp/src/apijson/demo/RequestUtil.java
@@ -35,38 +35,38 @@ public class RequestUtil {
 		data.setName("Tommy");// 测试necessaryColumns = [name,phone]通过
 		data.setSex(0);
 		data.setPhone("1234567890");// 测试necessaryColumns = [name,phone]通过
-		data.setHead("http://common.cnblogs.com/images/icon_weibo_24.png");
+		//		data.setHead("http://common.cnblogs.com/images/icon_weibo_24.png");
 		JSONRequest request = new JSONRequest();
 		request.put(data);
 		return request.setTag(User.class.getSimpleName());//;// 测试必须指定tag通过
 	}
-	
+
 	public static JSONObject newDeleteRequest() {
 		// 测试necessaryColumns = [id]通过
 		// 测试对象不存在通过，存在返回success通过
 		return new JSONRequest(new User(10000)).setTag(User.class.getSimpleName());//;// 测试必须指定tag通过
 	}
-	
+
 	public static JSONObject newPutRequest() {
 		User data = new User(38710);//);// 测试necessaryColumns = [id]通过
 		data.setName("Lemon");
 		//测试disallowColumns = [phone]通过 data.setPhone("1234567890");
-		
+
 		List<String> list = new ArrayList<String>();
 		list.add("http://static.oschina.net/uploads/user/1218/2437072_100.jpg?t=1461076033000");
 		list.add("http://common.cnblogs.com/images/icon_weibo_24.png");
 		list.add("http://static.oschina.net/uploads/user/585/1170143_50.jpg?t=1390226446000");
 		data.setPicture(JSON.toJSONString(list));//"\"" + JSON.toJSONString(new JSONObject("{\"id\":2}")) + "\"");//
-		
+
 		return new JSONRequest(data).setTag(User.class.getSimpleName());
 	}
 
-	
-	
+
+
 	public static JSONObject newSingleRequest() {
 		return new JSONRequest(new User(38710));
 	}
-	
+
 	public static JSONObject newColumnsRequest() {
 		JSONObject object = new JSONObject(new User(38710));
 		object.setColumns("id,name,phone");//StringUtil.getString(new String[]{"id", "name", "phone"}));//
@@ -79,7 +79,7 @@ public class RequestUtil {
 		request.put(Work.class.getSimpleName(), new JSONRequest("userId", "User/id"));
 		return request;
 	}
-	
+
 	public static JSONObject newArrayRequest() {
 		return new JSONRequest(new User()).toArray(5, 1, User.class.getSimpleName());
 	}
@@ -98,7 +98,7 @@ public class RequestUtil {
 	public static JSONObject newAccessErrorRequest() {
 		return new JSONRequest(new Wallet(38710));
 	}
-	
+
 	public static JSONObject newAccessPermittedRequest() {
 		JSONRequest request = new JSONRequest();
 		request.put(new Wallet().setUserId((long) 38710));
