13c3ee4c44bf2d22d039b2681e0bc737ebe0a7f0
TommyLemon <1184482681@qq.com>
2017-04-25 15:38:11 +0800

    Client:Test工程中AutoActivity新增[]解析java

--- APIJSON(Android)/APIJSON(ADT)/APIJSONTest/src/apijson/demo/ui/AutoActivity.java
+++ APIJSON(Android)/APIJSON(ADT)/APIJSONTest/src/apijson/demo/ui/AutoActivity.java
@@ -14,23 +14,20 @@ limitations under the License.*/
 
 package apijson.demo.ui;
 
-import static zuo.biao.apijson.StringUtil.bigAlphaPattern;
-
 import java.util.Set;
 
 import zuo.biao.apijson.JSON;
 import zuo.biao.apijson.JSONRequest;
 import zuo.biao.apijson.JSONResponse;
-import zuo.biao.apijson.Log;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import apijson.demo.R;
 import apijson.demo.StringUtil;
-import apijson.demo.model.User;
 
 import com.alibaba.fastjson.JSONObject;
 
@@ -62,12 +59,44 @@ public class AutoActivity extends Activity {
 
 
 
-		String request = "{\"User\":{\"id\":1,\"@column\":\"id,name\"},\"Moment\":{\"userId@\":\"/User/id\"}}";
+		String request = "{\"User\":{\"id\":1,\"@column\":\"id,name\"},\"[]\":{\"count\":3,\"page\":1,\"Moment\":{\"userId@\":\"User/id\"}}}";
 
 		tvAutoResult.setText(StringUtil.getString(JSON.format(request)));
 
+	}
 
 
+	public void create(View v) {
+		JSONRequest request = new JSONRequest();
+
+		//User<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		JSONRequest userRequest = new JSONRequest();
+		userRequest.put("id", 1);
+		userRequest.put("@column", "id,name");
+
+		request.put("User", userRequest);
+		//User>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+		//[]<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		JSONRequest item = new JSONRequest();
+
+		//Moment<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		JSONRequest momentRequest = new JSONRequest();
+		momentRequest.put("userId@", "User/id");
+
+		item.put("Moment", momentRequest);
+		//Moment>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+		request.add(item.toArray(0, 0));
+		//[]>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		
+		String json = StringUtil.getString(JSON.format(request));
+		Log.d(TAG, "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n request = \n" + json + "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		
+		tvAutoResult.setText(json);
 	}
 
 
@@ -78,8 +107,8 @@ public class AutoActivity extends Activity {
 	public void auto(String request) {
 		String response = parse("", JSON.parseObject(request)); //newObjectRequest(request);
 
-		Log.d(TAG, "\n request = \n" + request);
-		Log.d(TAG, "\n response = \n" + response);
+		Log.d(TAG, "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n request = \n" + request + "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		Log.d(TAG, "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n response = \n" + response + "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
 
 		tvAutoResult.setText(StringUtil.getString(response));
 	}
@@ -87,36 +116,18 @@ public class AutoActivity extends Activity {
 
 	public static final String NEWLINE = "\n";
 
-	private String newObjectRequest(String json) {
-		JSONRequest request = new JSONRequest();
-
-
-		//单个JSONObject<<<<<<<<<<<<<<<<<
-		JSONRequest userRequest = new JSONRequest();
-
-		User user = new User();
-		user.setId((long) 1);
-
-		userRequest.add(JSON.parseObject(user));
-
-		userRequest.setColumn("id,name");
-		//单个JSONObject>>>>>>>>>>>>>>>>>>
-
-
-		request.put("User", userRequest);
-
-
-		return JSON.format(request);
-	}
-
-
+	/**
+	 * @param name
+	 * @param request
+	 * @return
+	 */
 	public static String parse(final String name, final JSONObject request) {
-		Log.i(TAG, "parse  request = \n" + JSON.toJSONString(request));
+//		Log.i(TAG, "parse  request = \n" + JSON.toJSONString(request));
 		if (request == null || request.isEmpty()) {
-			Log.i(TAG, "parse  request == null || request.isEmpty() >> return request;");
+//			Log.i(TAG, "parse  request == null || request.isEmpty() >> return request;");
 			return null;
 		}
-		String parentKey = getTableKey(name);
+		String parentKey = isArrayKey(name) ? getItemKey(name) : getTableKey(name);
 
 
 		String response = NEWLINE + "JSONRequest " + parentKey + " = new JSONRequest();";
@@ -126,28 +137,44 @@ public class AutoActivity extends Activity {
 
 			Object value;
 			String pairKey;
-			String arrayKey;
 			for (String key : set) {
 				value = request.get(key);
 				pairKey = new String(key instanceof String ? "\"" + key + "\"" : key);
 				if (value instanceof JSONObject) {//APIJSON Array转为常规JSONArray
 					if (isArrayKey(key)) {//APIJSON Array转为常规JSONArray
-						//						arrayKey = key.substring(0, key.indexOf(JSONResponse.KEY_ARRAY));
+						response += NEWLINE + NEWLINE + "//" + key + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+						
+						int count = ((JSONObject) value).getIntValue(JSONRequest.KEY_COUNT);
+						int page = ((JSONObject) value).getIntValue(JSONRequest.KEY_PAGE);
+						((JSONObject) value).remove(JSONRequest.KEY_COUNT);
+						((JSONObject) value).remove(JSONRequest.KEY_PAGE);
+
+						response += parse(key, (JSONObject) value);
+
+
+						String prefix = key.substring(0, key.length() - 2);
+						response += NEWLINE + NEWLINE
+								+ parentKey + ".add(" +  getItemKey(key) + ".toArray("
+								+ count  + ", " + page + (prefix.isEmpty() ? "" : ", " + prefix) + "));";
+						
+						response += NEWLINE + "//" + key + ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + NEWLINE;
 					} else {//常规JSONObject，往下一级提取
+						response += NEWLINE + NEWLINE + "//" + key + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
 						
-						response += NEWLINE + parse(key, (JSONObject) value);
-						
-						response += NEWLINE + NEWLINE + parentKey + ".put(" + pairKey + ", " + getTableKey(key) + ");" + NEWLINE;
+						response += parse(key, (JSONObject) value);
+
+						response += NEWLINE + NEWLINE + parentKey + ".put(" + pairKey + ", " + getTableKey(key) + ");";
+						response += NEWLINE + "//" + key + ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + NEWLINE;
 					}
 				} else {//其它Object，直接填充
 					value = value instanceof String ? new String("\"" + value + "\"") : value;
-					
+
 					response += NEWLINE + parentKey + ".put(" + pairKey + ", " + value + ");";
 				}
 			}
 		}
 
-		Log.i(TAG, "parse  return response = \n" + response);
+//		Log.i(TAG, "parse  return response = \n" + response);
 		return response;
 	}
 
@@ -156,21 +183,55 @@ public class AutoActivity extends Activity {
 
 
 	/**获取Table变量名
-	 * @param key => StringUtil.getNoBlankString(key)
-	 * @return empty ? "list" : key + "List" 且首字母小写
+	 * @param key
+	 * @return empty ? "request" : key + "Request" 且首字母小写
 	 */
 	private static String getTableKey(String key) {
+		return addSuffix(key, "Request");
+	}
+	/**获取Table变量名
+	 * @param key
+	 * @return empty ? "request" : key + "Request" 且首字母小写
+	 */
+	private static String getItemKey(String key) {
+		return addSuffix(key.substring(0, key.length() - 2), "Item");
+	}
+	/**
+	 * @param key
+	 * @param suffix
+	 * @return key + suffix，第一个字母小写
+	 */
+	private static String addSuffix(String key, String suffix) {
 		key = StringUtil.getNoBlankString(key);
 		if (key.isEmpty()) {
-			return "request";
+			return firstCase(suffix);
+		}
+
+		return firstCase(key) + firstCase(suffix, true);
+	}
+	/**
+	 * @param key
+	 */
+	private static String firstCase(String key) {
+		return firstCase(key, false);
+	}
+	/**
+	 * @param key
+	 * @param upper
+	 * @return
+	 */
+	private static String firstCase(String key, boolean upper) {
+		key = StringUtil.getString(key);
+		if (key.isEmpty()) {
+			return "";
 		}
 
 		String first = key.substring(0, 1);
-		if (bigAlphaPattern.matcher(first).matches()) {
-			key = first.toLowerCase() + key.substring(1, key.length());
-		}
-		return key + "Request";
+		key = (upper ? first.toUpperCase() : first.toLowerCase()) + key.substring(1, key.length());
+
+		return key;
 	}
+
 
 
 	private static String getSimpleName(String arrayKey) {
