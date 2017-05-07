46254f52f7fc811340ef14e4185c76672424bbc1
TommyLemon <1184482681@qq.com>
2017-05-08 00:34:49 +0800

    Server:允许跨域请求

--- APIJSON(Server)/APIJSON(Eclipse_JEE)/src/main/java/zuo/biao/apijson/server/APIJSONApplication.java
+++ APIJSON(Server)/APIJSON(Eclipse_JEE)/src/main/java/zuo/biao/apijson/server/APIJSONApplication.java
@@ -16,6 +16,10 @@ package zuo.biao.apijson.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**application
  * @author Lemon
@@ -38,4 +42,29 @@ public class APIJSONApplication {
 			e.printStackTrace();
 		}
 	}
+	
+	
+
+      
+    /** 
+     * 跨域过滤器 
+     * @return 
+     */  
+    @Bean  
+    public CorsFilter corsFilter() {  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        source.registerCorsConfiguration("/**", buildConfig()); // 4  
+        return new CorsFilter(source);  
+    }  
+    /**CORS跨域配置
+     * @return
+     */
+	private CorsConfiguration buildConfig() {  
+        CorsConfiguration corsConfiguration = new CorsConfiguration();  
+        corsConfiguration.addAllowedOrigin("*");  
+        corsConfiguration.addAllowedHeader("*");  
+        corsConfiguration.addAllowedMethod("*");  
+        return corsConfiguration;  
+    }  
+	
 }
