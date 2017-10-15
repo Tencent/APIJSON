149960c1ac959a8ba48c6d0f800b44ae9581d77c
TommyLemon <1184482681@qq.com>
2017-10-15 15:54:22 +0800

    同步table；JSON类型不能为NULL

--- table/sys__Statistics.sql
+++ table/sys__Statistics.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
 --
--- Host: localhost    Database: sys
+-- Host: 39.108.143.172    Database: sys
 -- ------------------------------------------------------
--- Server version	5.7.16
+-- Server version	5.7.19-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-06  1:02:35
+-- Dump completed on 2017-10-15 15:52:45
