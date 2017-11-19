7b32b5af8656a1d86f1f2d9f3a83a85466b35ed2
TommyLemon <1184482681@qq.com>
2017-11-19 21:11:14 +0800

    更新table

--- table/sys_Praise.sql
+++ table/sys_Praise.sql
@@ -23,10 +23,12 @@ DROP TABLE IF EXISTS `Praise`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Praise` (
-  `id` bigint(15) NOT NULL COMMENT '唯一标识',
-  `userIdList` json NOT NULL COMMENT '用户id列表',
+  `id` bigint(15) NOT NULL AUTO_INCREMENT COMMENT '动态id',
+  `momentId` bigint(15) NOT NULL COMMENT '唯一标识',
+  `userId` bigint(15) NOT NULL COMMENT '用户id',
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '点赞时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='如果对Moment写安全要求高，可以将Moment内praiserUserIdList分离到Praise表中，作为userIdList。\n权限注解也改下：\n@MethodAccess(\n		PUT = {OWNER, ADMIN}\n		)\nclass Moment {\n       …\n}\n\n@MethodAccess(\n		PUT = {LOGIN, CONTACT, CIRCLE, OWNER, ADMIN}\n		)\n class Praise {\n       …\n }\n';
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='如果对Moment写安全要求高，可以将Moment内praiserUserIdList分离到Praise表中，作为userIdList。\n权限注解也改下：\n@MethodAccess(\n		PUT = {OWNER, ADMIN}\n		)\nclass Moment {\n       …\n}\n\n@MethodAccess(\n		PUT = {LOGIN, CONTACT, CIRCLE, OWNER, ADMIN}\n		)\n class Praise {\n       …\n }\n';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +37,7 @@ CREATE TABLE `Praise` (
 
 LOCK TABLES `Praise` WRITE;
 /*!40000 ALTER TABLE `Praise` DISABLE KEYS */;
-INSERT INTO `Praise` VALUES (12,'[38710, 82003, 70793, 93793, 82006, 82044, 82040, 82055, 82002, 90814, 82001]'),(15,'[82055, 82001, 82002]'),(32,'[38710, 82001, 82002]'),(58,'[38710, 82003, 82005, 93793, 82006, 82044, 82001]'),(170,'[82044, 82002, 82001]'),(235,'[]'),(301,'[38710, 93793, 82003, 82005, 82040, 82055, 82002, 82001]'),(371,'[90814, 82002, 93793, 82003, 82005, 82006, 82040, 82001]'),(470,'[]'),(511,'[70793, 93793, 82001]'),(543,'[]'),(551,'[]'),(594,'[38710, 82002]'),(595,'[70793, 82002, 82001]'),(704,'[82003, 82002, 82001]'),(1491200468898,'[]'),(1491277116776,'[38710, 82001]'),(1493835799335,'[82001, 82002]');
+INSERT INTO `Praise` VALUES (1,12,82001,'2017-11-19 13:02:30'),(2,15,82002,'2017-11-19 13:02:30'),(3,32,82003,'2017-11-19 13:02:30'),(4,58,82004,'2017-11-19 13:02:30'),(5,170,82005,'2017-11-19 13:02:30'),(6,235,82006,'2017-11-19 13:02:30'),(7,301,82007,'2017-11-19 13:02:30'),(8,371,82008,'2017-11-19 13:02:30'),(9,470,82009,'2017-11-19 13:02:30'),(10,511,82010,'2017-11-19 13:02:30'),(11,543,82011,'2017-11-19 13:02:30'),(12,551,82012,'2017-11-19 13:02:30'),(13,594,82013,'2017-11-19 13:02:30'),(14,595,82014,'2017-11-19 13:02:30'),(15,704,82015,'2017-11-19 13:02:30'),(16,1491200468898,82016,'2017-11-19 13:02:30'),(17,1491277116776,82017,'2017-11-19 13:02:30'),(18,1493835799335,82018,'2017-11-19 13:02:30');
 /*!40000 ALTER TABLE `Praise` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-12 21:45:15
+-- Dump completed on 2017-11-19 21:09:15
