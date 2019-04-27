c1c311f805b4a404181b9108f929b68da943cc36
TommyLemon <1184482681@qq.com>
2019-04-27 12:14:37 +0800

    Server:MAX_SQL_COUNT提高到1000条；打包apijson-orm-3.5.3.jar

--- APIJSON-Java-Server/APIJSONORM/src/main/java/zuo/biao/apijson/server/Parser.java
+++ APIJSON-Java-Server/APIJSONORM/src/main/java/zuo/biao/apijson/server/Parser.java
@@ -29,7 +29,7 @@ public interface Parser<T> {
 	int MAX_QUERY_PAGE = 100;
 	int MAX_QUERY_COUNT = 100;
 	int MAX_UPDATE_COUNT = 10;
-	int MAX_SQL_COUNT = 100;
+	int MAX_SQL_COUNT = 1000;
 	int MAX_OBJECT_COUNT = 5;
 	int MAX_ARRAY_COUNT = 5;
 	int MAX_QUERY_DEPTH = 5;
