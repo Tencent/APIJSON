e4975c73300bd9ff2d5c74e910f81b6580a5123d
TommyLemon <1184482681@qq.com>
2019-06-24 22:07:58 +0800

    Server:解决 Oracle 版 oracle jdbc 依赖下载不了；注释报错且未用上的方法

--- APIJSON-Java-Server/APIJSONBootOracle/src/main/java/apijson/demo/server/DemoSQLExecutor.java
+++ APIJSON-Java-Server/APIJSONBootOracle/src/main/java/apijson/demo/server/DemoSQLExecutor.java
@@ -88,28 +88,28 @@ public class DemoSQLExecutor extends AbstractSQLExecutor {
 	}
 
 	
-	@Override
-	protected Object getValue(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition,
-			JSONObject table, int columnIndex, Map<String, JSONObject> childMap) throws Exception {
-		Object value = super.getValue(config, rs, rsmd, tablePosition, table, columnIndex, childMap);
-		
-		if (value instanceof Blob) {
-			value = new String(((Blob) value).getBytes(1, (int) ((Blob) value).length()), "UTF-8");
-		}
-		else if (value instanceof Clob) {
-			
-			StringBuffer sb = new StringBuffer(); 
-			BufferedReader br = new BufferedReader(((Clob) value).getCharacterStream()); 
-			String s = br.readLine();
-			while (s != null) {
-				sb.append(s); 
-				s = br.readLine(); 
-			}
-			value = sb.toString();
-		}
-		
-		return value;
-	}
+//	@Override
+//	protected Object getValue(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition,
+//			JSONObject table, int columnIndex, Map<String, JSONObject> childMap) throws Exception {
+//		Object value = super.getValue(config, rs, rsmd, tablePosition, table, columnIndex, childMap);
+//		
+//		if (value instanceof Blob) {
+//			value = new String(((Blob) value).getBytes(1, (int) ((Blob) value).length()), "UTF-8");
+//		}
+//		else if (value instanceof Clob) {
+//			
+//			StringBuffer sb = new StringBuffer(); 
+//			BufferedReader br = new BufferedReader(((Clob) value).getCharacterStream()); 
+//			String s = br.readLine();
+//			while (s != null) {
+//				sb.append(s); 
+//				s = br.readLine(); 
+//			}
+//			value = sb.toString();
+//		}
+//		
+//		return value;
+//	}
 
 	/**关闭连接，释放资源
 	 */
