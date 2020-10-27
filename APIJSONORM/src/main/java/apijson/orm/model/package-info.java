b249994a8afa85c64aa9f108f1003b749b1df553
TommyLemon <1184482681@qq.com>
2020-10-27 21:23:55 +0800

    所有代码文件版权归属改为腾讯公司 THL A29 Limited

--- APIJSONORM/src/main/java/apijson/orm/model/package-info.java
+++ APIJSONORM/src/main/java/apijson/orm/model/package-info.java
@@ -1,3 +1,8 @@
+/*Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+
+This source code is licensed under the Apache License Version 2.0.*/
+
+
 /**
  * models for special tables
  */
