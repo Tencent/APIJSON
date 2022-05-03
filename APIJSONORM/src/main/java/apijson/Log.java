95432dde2c7a1f5147f9fe2f77f5500d33c651d0
TommyLemon <1184482681@qq.com>
2022-05-04 01:52:49 +0800

    升级版本号为 5.1.0；删除不再需要的依赖 javax.activation

--- APIJSONORM/src/main/java/apijson/Log.java
+++ APIJSONORM/src/main/java/apijson/Log.java
@@ -14,7 +14,7 @@ public class Log {
 
 	public static boolean DEBUG = true;
 	
-	public static final String VERSION = "5.0.0";
+	public static final String VERSION = "5.1.0";
 	public static final String KEY_SYSTEM_INFO_DIVIDER = "---|-----APIJSON SYSTEM INFO-----|---";
 
 	//默认的时间格式
