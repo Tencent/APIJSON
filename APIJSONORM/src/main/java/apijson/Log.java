1eb860968c4628dc1588f5c5276d5a9625207963
TommyLemon <1184482681@qq.com>
2022-10-17 21:01:34 +0800

    Update Log.java

--- APIJSONORM/src/main/java/apijson/Log.java
+++ APIJSONORM/src/main/java/apijson/Log.java
@@ -17,16 +17,16 @@ public class Log {
 	public static final String VERSION = "5.3.0";
 	public static final String KEY_SYSTEM_INFO_DIVIDER = "\n---|-----APIJSON SYSTEM INFO-----|---\n";
 
-  public static final String OS_NAME;
-  public static final String OS_VERSION;
-  public static final String OS_ARCH;
-  public static final String JAVA_VERSION;
-  static {
-    OS_NAME = System.getProperty("os.name");
-    OS_VERSION = System.getProperty("os.version");
-    OS_ARCH = System.getProperty("os.arch");
-    JAVA_VERSION = System.getProperty("java.version");
-  }
+	public static final String OS_NAME;
+	public static final String OS_VERSION;
+	public static final String OS_ARCH;
+	public static final String JAVA_VERSION;
+	static {
+		OS_NAME = System.getProperty("os.name");
+		OS_VERSION = System.getProperty("os.version");
+		OS_ARCH = System.getProperty("os.arch");
+		JAVA_VERSION = System.getProperty("java.version");
+	}
 
 
   //默认的时间格式
