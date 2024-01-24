c0c4af34af3bb93c96d1854d82e2a2dcbee64b36
TommyLemon <1184482681@qq.com>
2024-01-24 22:48:15 +0800

    Update Log.java

--- APIJSONORM/src/main/java/apijson/Log.java
+++ APIJSONORM/src/main/java/apijson/Log.java
@@ -14,7 +14,7 @@ public class Log {
 
 	public static boolean DEBUG = true;
 
-	public static final String VERSION = "6.4.0";
+	public static final String VERSION = "6.4.3";
 	public static final String KEY_SYSTEM_INFO_DIVIDER = "\n---|-----APIJSON SYSTEM INFO-----|---\n";
 
 	public static final String OS_NAME;
