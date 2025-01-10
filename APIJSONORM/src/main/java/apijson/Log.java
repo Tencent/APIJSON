2cb277e12b71908ccd309de06d8c48d33981f2fe
TommyLemon <tommylemon@qq.com>
2025-01-11 00:44:43 +0800

    升级版本至 7.3.0

--- APIJSONORM/src/main/java/apijson/Log.java
+++ APIJSONORM/src/main/java/apijson/Log.java
@@ -14,7 +14,7 @@ public class Log {
 
 	public static boolean DEBUG = true;
 
-	public static final String VERSION = "7.2.0";
+	public static final String VERSION = "7.3.0";
 	public static final String KEY_SYSTEM_INFO_DIVIDER = "\n---|-----APIJSON SYSTEM INFO-----|---\n";
 
 	public static final String OS_NAME;
