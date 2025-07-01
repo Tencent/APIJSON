f6703c9c061569c8397ef4fe4f66b560910f450d
wuzeng <wuzeng@chalco-steering.com>
2025-07-01 20:30:57 +0800

    fix: 在Head Method，PG数据库 left join 副表 多个业务键关联，出现找不到column

--- APIJSONORM/src/main/java/apijson/orm/AbstractSQLConfig.java
+++ APIJSONORM/src/main/java/apijson/orm/AbstractSQLConfig.java
@@ -6324,7 +6324,10 @@ public abstract class AbstractSQLConfig<T, M extends Map<String, Object>, L exte
 				List<String> column = onList == null ? null : new ArrayList<>(onList.size());
 				if (column != null) {
 					for (On on : onList) {
-						column.add(on.getKey());
+						//解决 pg  如果只查询关联键，会报找不到column的错误
+						///* SELECT  count(*)  AS count  FROM sys.Moment AS Moment
+						//			   LEFT JOIN ( SELECT *  FROM sys.Comment ) AS Comment ON Comment.momentId = Moment.id LIMIT 1 OFFSET 0 */
+						//column.add(on.getKey());
 					}
 				}
 
