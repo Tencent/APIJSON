bd5151b60ab9b5194ea55607a10f5d09fe420d51
TommyLemon <tommylemon@qq.com>
2026-03-29 16:15:04 +0800

    结果集容量预估：最低开启阈值默认设置为 2^16 - 1 = 65535，ArrayList 最多扩容 5 次

--- APIJSONORM/src/main/java/apijson/orm/AbstractSQLExecutor.java
+++ APIJSONORM/src/main/java/apijson/orm/AbstractSQLExecutor.java
@@ -158,6 +158,8 @@ public abstract class AbstractSQLExecutor<T, M extends Map<String, Object>, L ex
 		return rs;
 	}
 
+	public static int MIN_OPTIMIZE_CAPACITY = 2^16 - 1;
+
 	/**执行SQL
 	 * @param config
 	 * @return
@@ -330,7 +332,7 @@ public abstract class AbstractSQLExecutor<T, M extends Map<String, Object>, L ex
 						}
 						else {  // 预估容量
 							capacity = config.getCount() <= 0 ? AbstractParser.MAX_QUERY_COUNT : config.getCount();
-							if (capacity > 100) {
+							if (capacity > MIN_OPTIMIZE_CAPACITY) {
 								// 有 WHERE 条件，条件越多过滤数据越多，暂时不考虑 @combine:"a | (b & !c)" 里面 | OR 和 ! NOT 条件，太复杂也不是很必要
 								Map<String, List<String>> combine = config.getCombineMap();
 
@@ -354,14 +356,15 @@ public abstract class AbstractSQLExecutor<T, M extends Map<String, Object>, L ex
 								Map<String, Object> having = config.getHaving();
 								int havingCount = having == null ? 0 : having.size();
 
-								capacity /= Math.pow(1.5, Math.log10(capacity)
+								double cap = capacity / Math.pow(1.5, Math.log10(capacity)
 										+ andCondCount
 										+ ((orCondCount <= 0 ? 0 : 2.0d/orCondCount)  // 1: 2.3, 2: 1.5, 3: 1.3, 4: 1.23, 5: 1.18
 										+ (notCondCount/5.0d)  // 1: 1.08, 2: 1.18, 3: 1.28, 4: 1.38, 1.50
 										+ (groupCount <= 0 ? 0 : 10.0d/groupCount))  // 1: 57.7, 7.6, 3: 3.9, 4: 2.8, 5: 2.3
 										+ havingCount
 										);
-								capacity += 1;  // 避免正好比需要容量少一点点导致多一次扩容，大量数据 System.arrayCopy
+								cap = Math.max(MIN_OPTIMIZE_CAPACITY, Math.max(cap, capacity/Math.pow(1.5, 5))); // 1/(1.5^5) = 0.13
+								capacity = (int) (cap + 1);  // 避免正好比需要容量少一点点导致多一次扩容，大量数据 System.arrayCopy
 							}
 						}
 					}
